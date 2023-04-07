7e52c6e0e886bc0b1aadfb900ca288c2d08d6931
Haris Adnan <haris.adnan@duke.edu>
2023-04-07 16:16:54 -0400

    Initial Commit

---
+++ PitchPerfect/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PitchPerfect
+//
+//  Created by Haris Adnan on 4/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PitchPerfect/PitchPerfectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchPerfectApp.swift
+//  PitchPerfect
+//
+//  Created by Haris Adnan on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct PitchPerfectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
